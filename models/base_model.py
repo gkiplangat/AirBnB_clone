c0e30b2f5e92df25c05165b4e67446d626241618
gkiplangat <gkiplangat>
2023-08-12 15:56:51 +0300

    creating base files

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This module contains the BaseModel class that defines all common
+attributes/methods for other classes.
+"""
+from datetime import datetime
+import uuid
+
+
+class BaseModel:
+    """This defines a BaseModel class"""
+    def __init__(self) -> None:
+        self.id: str = str(uuid.uuid4())
+        self.created_at: datetime = datetime.now()
+        self.updated_at: datetime = datetime.now()
+
+    def __str__(self) -> str:
+        return f"{[self.__class__.__name__]} ({self.id}) {self.__dict__}"
+
+    def save(self) -> None:
+        """
+        Save the object by updating the `updated_at` attribute with
+        the current datetime. This function does not take any
+        parameters and does not return anything.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self) -> dict:
+        """
+        Converts the object to a dictionary representation.
+        Returns:
+            dict: A dictionary representation of the object, including
+            the class name, created_at, and updated_at attributes.
+        """
+        form: str = "%Y-%m-%dT%H:%M:%S.%f"
+        properties: dict = self.__dict__
+        properties["__class__"] = self.__class__.__name__
+        properties["created_at"] = self.created_at.strftime(form)
+        properties["updated_at"] = self.updated_at.strftime(form)
+        # properties["created_at"] = datetime.isoformat(self.updated_at, form)
+        # properties["updated_at"] = datetime.isoformat(self.updated_at, form)
+
+        return properties
