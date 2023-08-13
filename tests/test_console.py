f9e5be6b5d303f7311918a6e0872320698d39809
gkiplangat <gkiplangat>
2023-08-13 16:55:30 +0300

    feat:console test cases

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,93 +1,46 @@
-#!/usr/bin/python3
-"""Defines unittests for console.py.
-Unittest classes:
-    TestHBNBCommand_prompting
-    TestHBNBCommand_help
-    TestHBNBCommand_exit
-    TestHBNBCommand_create
-    TestHBNBCommand_show
-    TestHBNBCommand_all
-    TestHBNBCommand_destroy
-    TestHBNBCommand_update
-"""
-import os
 import unittest
-from io import StringIO
 from unittest.mock import patch
-
-from console import HBNBCommand
-from models import storage
-from models.engine.file_storage import FileStorage
-
-
-class TestHBNBCommandPrompt(unittest.TestCase):
-    """
-    Unittests for testing prompting of the HBNB command interpreter.
-    """
-
-    def test_prompt_string(self) -> None:
-        """..."""
-        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
-
-    def test_empty_line(self) -> None:
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd(""))
-            self.assertEqual("", output.getvalue().strip())
+import io
+import sys
+import console  # Import the console module
 
 
-class TestHBNBCommandHelp(unittest.TestCase):
-    """Unittests for testing help messages of the HBNB command interpreter."""
+class TestHBNBCommand(unittest.TestCase):
+    def setUp(self):
+        self.console = console.HBNBCommand()
+        self.patcher = patch('builtins.input')
+        self.mock_input = self.patcher.start()
 
-    def test_help_quit(self) -> None:
-        """..."""
-        hhbelp = "Quit command to exit the program"
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("help quit"))
-            self.assertEqual(hhbelp, output.getvalue().strip())
+    def tearDown(self):
+        self.patcher.stop()
 
-    def test_help_create(self) -> None:
-        """..."""
-        hhbelp = "Usage: create <class_name>"
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("help create"))
-            self.assertEqual(hhbelp, output.getvalue().strip())
+    def test_quit_command(self):
+        self.assertTrue(self.console.onecmd("quit"))
 
-    def test_help_EOF(self) -> None:
-        """..."""
-        hhbelp = "Cleanly exits from the interpreter"
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("help EOF"))
-            self.assertEqual(hhbelp, output.getvalue().strip())
+    def test_EOF_command(self):
+        self.assertTrue(self.console.onecmd("EOF"))
 
-    def test_help_show(self) -> None:
-        hhbelp = "Usage: show <class_name> <id> or <class>.show(<id>)"
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("help show"))
-            self.assertEqual(hhbelp, output.getvalue().strip())
+    def test_empty_line(self):
+        self.assertIsNone(self.console.onecmd(""))
 
-    def test_help_destroy(self) -> None:
-        hhbelp = (
-            "Usage: destroy <class_name> <id> or <class>.destroy(<id>)\n"
-            "Removes the specified class"
-        )
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("help destroy"))
-            self.assertEqual(hhbelp, output.getvalue().strip())
+    def test_help_command(self):
+        expected_output = (
+            "Documented commands (type help <topic>):\n"
+            "========================================\n"
+            "EOF  help  quit"
+            )
 
-    def test_help_all(self) -> None:
-        hhbelp = "Usage: all or all <class> or <class>.all()"
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("help all"))
-            self.assertEqual(hhbelp, output.getvalue().strip())
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            self.assertFalse(self.console.onecmd("help"))
+            output = mock_stdout.getvalue().strip()
+            self.assertEqual(output, expected_output)
+
+    def test_help_quit_command(self):
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            self.assertFalse(self.console.onecmd("help quit"))
+            output = mock_stdout.getvalue().strip()
+            self.assertEqual(output, "Quit command to exit the program")
 
 
-class TestHBNBCommandExit(unittest.TestCase):
-    """Unittests for testing exiting from the HBNB command interpreter."""
-
-    def test_quit_exits(self) -> None:
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertTrue(HBNBCommand().onecmd("quit"))
-
-    def test_EOF_exits(self) -> None:
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertTrue(HBNBCommand().onecmd("EOF"))
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,38 +1,142 @@
+#!/usr/bin/python3
+"""Defines unittests for models/engine/file_storage.py.
+Unittest classes:
+    TestFileStorageInstantiation
+    TestFileStorageMethods
+"""
+
+import contextlib
+import os
 import unittest
-from unittest.mock import patch
-import io
-import sys
-import console  # Import the console module
 
-class TestHBNBCommand(unittest.TestCase):
-    def setUp(self):
-        self.console = console.HBNBCommand()
-        self.patcher = patch('builtins.input')
-        self.mock_input = self.patcher.start()
+import models
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
-    def tearDown(self):
-        self.patcher.stop()
 
-    def test_quit_command(self):
-        self.assertTrue(self.console.onecmd("quit"))
 
-    def test_EOF_command(self):
-        self.assertTrue(self.console.onecmd("EOF"))
+class TestFileStorageInstantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the FileStorage class."""
 
-    def test_empty_line(self):
-        self.assertIsNone(self.console.onecmd(""))
+    def test_file_storage_instantiation_no_args(self) -> None:
+        self.assertEqual(type(FileStorage()), FileStorage)
 
-    def test_help_command(self):
-        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
-            self.assertFalse(self.console.onecmd("help"))
-            output = mock_stdout.getvalue().strip()
-            self.assertEqual(output, "Documented commands (type help <topic>):\n========================================\nEOF  help  quit")
+    def test_file_storage_instantiation_with_arg(self) -> None:
+        with self.assertRaises(TypeError):
+            FileStorage(None)
 
-    def test_help_quit_command(self):
-        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
-            self.assertFalse(self.console.onecmd("help quit"))
-            output = mock_stdout.getvalue().strip()
-            self.assertEqual(output, "Quit command to exit the program")
+    def test_file_storage_file_path_private_is_str(self) -> None:
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
 
-if __name__ == '__main__':
+    def testFileStorage_objects_private_is_dict(self) -> None:
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+    def test_storage_initializes(self) -> None:
+        self.assertEqual(type(models.storage), FileStorage)
+
+
+class TestFileStorageMethods(unittest.TestCase):
+    """Unittests for testing methods of the FileStorage class."""
+
+    def setUp(self) -> None:
+        self.base = BaseModel()
+        self.user = User()
+        self.state = State()
+        self.place = Place()
+        self.city = City()
+        self.amenity = Amenity()
+        self.review = Review()
+        models.storage.new(self.base)
+        models.storage.new(self.user)
+        models.storage.new(self.state)
+        models.storage.new(self.place)
+        models.storage.new(self.city)
+        models.storage.new(self.amenity)
+        models.storage.new(self.review)
+        # return super().setUp()
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        with contextlib.suppress(IOError):
+            os.rename("file.json", "tmp")
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        with contextlib.suppress(IOError):
+            os.remove("file.json")
+        with contextlib.suppress(IOError):
+            os.rename("tmp", "file.json")
+        FileStorage._FileStorage__objects = {}
+
+    def test_all(self) -> None:
+        self.assertEqual(dict, type(models.storage.all()))
+
+    def test_all_with_arg(self) -> None:
+        with self.assertRaises(TypeError):
+            models.storage.all(None)
+
+    def test_new(self) -> None:
+        self.assertIn(f"BaseModel.{self.base.id}", models.storage.all().keys())
+        self.assertIn(self.base, models.storage.all().values())
+        self.assertIn(f"User.{self.user.id}", models.storage.all().keys())
+        self.assertIn(self.user, models.storage.all().values())
+        self.assertIn(f"State.{self.state.id}", models.storage.all().keys())
+        self.assertIn(self.state, models.storage.all().values())
+        self.assertIn(f"Place.{self.place.id}", models.storage.all().keys())
+        self.assertIn(self.place, models.storage.all().values())
+        self.assertIn(f"City.{self.city.id}", models.storage.all().keys())
+        self.assertIn(self.city, models.storage.all().values())
+        self.assertIn(
+            f"Amenity.{self.amenity.id}", models.storage.all().keys()
+        )
+        self.assertIn(self.amenity, models.storage.all().values())
+        self.assertIn(f"Review.{self.review.id}", models.storage.all().keys())
+        self.assertIn(self.review, models.storage.all().values())
+
+    def test_new_with_args(self) -> None:
+        with self.assertRaises(TypeError):
+            models.storage.new(BaseModel(), 1)
+
+    def test_new_with_none(self) -> None:
+        with self.assertRaises(AttributeError):
+            models.storage.new(None)
+
+    def test_save(self) -> None:
+        models.storage.save()
+        save_text = ""
+        with open("file.json", mode="r", encoding="utf-8") as file:
+            save_text = file.read()
+            self.assertIn(f"BaseModel.{self.base.id}", save_text)
+            self.assertIn(f"User.{self.user.id}", save_text)
+            self.assertIn(f"State.{self.state.id}", save_text)
+            self.assertIn(f"Place.{self.place.id}", save_text)
+            self.assertIn(f"City.{self.city.id}", save_text)
+            self.assertIn(f"Amenity.{self.amenity.id}", save_text)
+            self.assertIn(f"Review.{self.review.id}", save_text)
+
+    def test_save_with_arg(self) -> None:
+        with self.assertRaises(TypeError):
+            models.storage.save(None)
+
+    def test_reload(self) -> None:
+        models.storage.save()
+        models.storage.reload()
+        objs = FileStorage._FileStorage__objects
+        self.assertIn(f"BaseModel.{self.base.id}", objs)
+        self.assertIn(f"User.{self.user.id}", objs)
+        self.assertIn(f"State.{self.state.id}", objs)
+        self.assertIn(f"Place.{self.place.id}", objs)
+        self.assertIn(f"City.{self.city.id}", objs)
+        self.assertIn(f"Amenity.{self.amenity.id}", objs)
+        self.assertIn(f"Review.{self.review.id}", objs)
+
+    def test_reload_no_file(self) -> None:
+        self.assertIsNone(models.storage.reload())
+
+    def test_reload_with_arg(self) -> None:
+        with self.assertRaises(TypeError):
+            models.storage.reload(None)
+
+
+if __name__ == "__main__":
     unittest.main()
