1f9146e825ffd31d1548d1893ec0ca006be0ef7f
gkiplangat <gkiplangat>
2023-08-13 18:31:03 +0300

    Revert "feat:corrected the base model"
    This reverts commit 037f3be87ac288a4bfd1dc4aadf9777302cf9037.

--- models/base_model.py
+++ models/base_model.py
@@ -4,27 +4,25 @@ import uuid
 from datetime import datetime
 
 from models import storage
-import models
 
 
 class BaseModel:
-    def __init__(self, *args: tuple, **kwargs: dict) -> None:
-        """..."""
-        self.id: str = str(uuid.uuid4())
-        self.created_at: datetime = datetime.now()
-        self.updated_at: datetime = self.created_at
-
+    def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                if key in ["created_at", "updated_at"]:
-                    val = datetime.strptime(str(value), "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, val)
-                else:
+                if key != "__class__":
                     setattr(self, key, value)
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(
+                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                storage.new(self)
         else:
-            models.storage.new(self)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return "[{}] ({}) {}".format(
