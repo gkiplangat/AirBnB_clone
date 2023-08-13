037f3be87ac288a4bfd1dc4aadf9777302cf9037
gkiplangat <gkiplangat>
2023-08-13 17:25:00 +0300

    feat:corrected the base model

--- models/base_model.py
+++ models/base_model.py
@@ -4,25 +4,27 @@ import uuid
 from datetime import datetime
 
 from models import storage
+import models
 
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: tuple, **kwargs: dict) -> None:
+        """..."""
+        self.id: str = str(uuid.uuid4())
+        self.created_at: datetime = datetime.now()
+        self.updated_at: datetime = self.created_at
+
         if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
+                if key == "__class__":
+                    continue
+                if key in ["created_at", "updated_at"]:
+                    val = datetime.strptime(str(value), "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, val)
+                else:
                     setattr(self, key, value)
-            if "created_at" in kwargs:
-                self.created_at = datetime.strptime(
-                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            if "updated_at" in kwargs:
-                self.updated_at = datetime.strptime(
-                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                storage.new(self)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(
