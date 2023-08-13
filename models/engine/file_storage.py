af3517371887577ae54aa96def71469196927b72
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-08-13 00:38:04 -0700

    Changed FileStorage to self

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,32 +1,38 @@
+#!/usr/bin/python3
 import json
 
 
 class FileStorage:
+    """
+        serializes instances to a JSON file and 
+        deserializes JSON file to instances
+        
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         data = {}
-        for key, obj in FileStorage.__objects.items():
+        for key, obj in self.__objects.items():
             data[key] = obj.to_dict()
-        with open(FileStorage.__file_path, 'w') as file:
+        with open(self.__file_path, 'w') as file:
             json.dump(data, file)
 
     def reload(self):
         try:
-            with open(FileStorage.__file_path, 'r') as file:
+            with open(self.__file_path, 'r') as file:
                 data = json.load(file)
                 for key, value in data.items():
                     cls_name = value['__class__']
                     cls = globals()[cls_name]
                     obj = cls(**value)
-                    FileStorage.__objects[key] = obj
+                    self.__objects[key] = obj
         except FileNotFoundError:
             pass
