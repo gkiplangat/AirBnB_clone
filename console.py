f665900002b60ce1b8073307f044486d6b6aaca8
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-08-13 12:58:53 +0300

    Added onto console.oy

--- console.py
+++ console.py
@@ -13,7 +13,18 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, arg):
         """Exit the program by pressing Ctrl+D"""
         return True
+    
+    def help_quit(self):
+        """Help guide for quit command"""
+        print('Quit command to exit the program')
 
+    def help_EOF(self):
+        """Help guide for EOF command"""
+        print('EOF command to exit the program')
+
+    def emptyline(self):
+        """Handles empty lines"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
