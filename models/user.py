adf425ca391c36871ca1605883ee1b38f46bce43
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-08-15 16:09:34 +0300

    Created the User Module

---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env/python3
+
+""" 
+    Task: 8. First User
+    User Module
+"""
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ User class inherits from BaseModel"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    
